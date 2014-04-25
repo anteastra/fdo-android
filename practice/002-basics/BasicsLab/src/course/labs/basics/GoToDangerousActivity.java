200d4b1da84f749bf2ade2f257a8fb9cba190822
anteastra <anteastra@gmail.com>
2014-04-26 02:50:30 +0700

    commit 001 and 002 and 003 tasks

---
+++ practice/002-basics/BasicsLab/src/course/labs/basics/GoToDangerousActivity.java
@@ -0,0 +1,45 @@
+package course.labs.basics;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import course.labs.basics.R;
+
+public class GoToDangerousActivity extends Activity {
+	
+	private static final String TAG = "Lab-Basics";
+
+	private static final String DANGEROUS_ACTIVITY_ACTION = "course.labs.permissions.DANGEROUS_ACTIVITY";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.go_to_dangerous_activity);
+
+		Button startDangerousActivityButton = (Button) findViewById(R.id.start_dangerous_activity_button);
+		startDangerousActivityButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				startDangerousActivity();
+
+			}
+		});
+
+	}
+
+	private void startDangerousActivity() {
+
+		Log.i(TAG, "Entered startDangerousActivity()");
+
+		startActivity(new Intent("DANGEROUS_ACTIVITY_ACTION"));
+		
+
+	}
+
+}
---
+++ practice/002-basics/BasicsLabTest/src/course/labs/permissionslab/test/TestDangerousApp.java
@@ -0,0 +1,52 @@
+package course.labs.permissionslab.test;
+
+import course.labs.basics.ActivityLoaderActivity;
+import com.robotium.solo.*;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class TestDangerousApp extends ActivityInstrumentationTestCase2<ActivityLoaderActivity> {
+  	private Solo solo;
+  	
+  	public TestDangerousApp() {
+		super(ActivityLoaderActivity.class);
+  	}
+
+  	public void setUp() throws Exception {
+		solo = new Solo(getInstrumentation());
+		getActivity();
+  	}
+  
+   	@Override
+   	public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+  	}
+  
+	public void testRun() {
+		
+		int timeout = 5;
+		
+		// Wait for activity: 'course.labs.permissionslab.ActivityLoaderActivity'
+		assertTrue("course.labs.permissionslab.ActivityLoaderActivity is not found!", solo.waitForActivity(course.labs.basics.ActivityLoaderActivity.class));
+
+		// Click on Bookmarks Activity
+		solo.clickOnView(solo.getView(course.labs.basics.R.id.start_bookmarks_button));
+
+		// Wait for activity: 'course.labs.permissionslab.BookmarksActivity'
+		assertTrue("course.labs.permissionslab.BookmarksActivity is not found!", solo.waitForActivity(course.labs.basics.BookmarksActivity.class));
+
+		// Click on Go To DangerousActivity
+		solo.clickOnView(solo.getView(course.labs.basics.R.id.go_to_dangerous_activity_button));
+		
+
+		// Wait for activity: 'course.labs.permissionslab.GoToDangerousActivity'
+		assertTrue("course.labs.permissionslab.GoToDangerousActivity is not found!", solo.waitForActivity(course.labs.basics.GoToDangerousActivity.class));
+		
+		// Assert that: 'This button will load a Dangerous Level activity' is shown
+		assertTrue("'This button will load a Dangerous Level activity' is not shown!", solo.waitForText(java.util.regex.Pattern.quote("This button will load a Dangerous Level activity")));
+		
+		// Click on Start Dangerous Activity
+		solo.clickOnView(solo.getView(course.labs.basics.R.id.start_dangerous_activity_button));
+		
+	}
+}
