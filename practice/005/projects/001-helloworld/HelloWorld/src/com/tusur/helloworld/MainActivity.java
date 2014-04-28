abb557134c2798d363b4eef6d5ace7ea473e865d
anteastra <anteastra@gmail.com>
2014-04-29 01:20:32 +0700

    add practice 004 005

---
+++ practice/005/projects/001-helloworld/HelloWorld/src/com/tusur/helloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package com.tusur.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView view = new TextView(this);
+        view.setText(getString(R.string.hello_world) +"\n" +
+        		"Привет, слушателям курса по Android!");
+        setContentView(view);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
